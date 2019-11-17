9db1c402755808ad1ffd47267e1ebb1ca48e5a60
ichidown <ichidown@gmail.com>
2019-11-17 18:59:25 +0100

    Initial commit

---
+++ transferGestionStockMetalMind/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace transferGestionStockMetalMind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MonthsComboBoxEnd = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MonthsComboBoxStart = new System.Windows.Forms.ComboBox();
+            this.TransferBtn = new System.Windows.Forms.Button();
+            this.ChantierBtn = new System.Windows.Forms.Button();
+            this.MMBtn = new System.Windows.Forms.Button();
+            this.ChantierPathTxt = new System.Windows.Forms.TextBox();
+            this.MMPathTxt = new System.Windows.Forms.TextBox();
+            this.ConsoleListBox = new System.Windows.Forms.ListBox();
+            this.BGWorker = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.MonthsListView = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MonthsComboBoxEnd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.MonthsComboBoxStart);
+            this.groupBox1.Controls.Add(this.TransferBtn);
+            this.groupBox1.Controls.Add(this.ChantierBtn);
+            this.groupBox1.Controls.Add(this.MMBtn);
+            this.groupBox1.Controls.Add(this.ChantierPathTxt);
+            this.groupBox1.Controls.Add(this.MMPathTxt);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(495, 191);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choisissez les dossiers MetalMind et Chantiers";
+            // 
+            // MonthsComboBoxEnd
+            // 
+            this.MonthsComboBoxEnd.FormattingEnabled = true;
+            this.MonthsComboBoxEnd.Location = new System.Drawing.Point(354, 106);
+            this.MonthsComboBoxEnd.Name = "MonthsComboBoxEnd";
+            this.MonthsComboBoxEnd.Size = new System.Drawing.Size(134, 26);
+            this.MonthsComboBoxEnd.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "fin mois";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "debut mois";
+            // 
+            // MonthsComboBoxStart
+            // 
+            this.MonthsComboBoxStart.FormattingEnabled = true;
+            this.MonthsComboBoxStart.Location = new System.Drawing.Point(102, 106);
+            this.MonthsComboBoxStart.Name = "MonthsComboBoxStart";
+            this.MonthsComboBoxStart.Size = new System.Drawing.Size(134, 26);
+            this.MonthsComboBoxStart.TabIndex = 7;
+            // 
+            // TransferBtn
+            // 
+            this.TransferBtn.Location = new System.Drawing.Point(19, 146);
+            this.TransferBtn.Name = "TransferBtn";
+            this.TransferBtn.Size = new System.Drawing.Size(469, 29);
+            this.TransferBtn.TabIndex = 6;
+            this.TransferBtn.Text = "Transferer les donnees vers les chantiers";
+            this.TransferBtn.UseVisualStyleBackColor = true;
+            this.TransferBtn.Click += new System.EventHandler(this.TransferBtn_Click);
+            // 
+            // ChantierBtn
+            // 
+            this.ChantierBtn.Location = new System.Drawing.Point(316, 71);
+            this.ChantierBtn.Name = "ChantierBtn";
+            this.ChantierBtn.Size = new System.Drawing.Size(172, 26);
+            this.ChantierBtn.TabIndex = 4;
+            this.ChantierBtn.Text = "Dossier chantiers";
+            this.ChantierBtn.UseVisualStyleBackColor = true;
+            this.ChantierBtn.Click += new System.EventHandler(this.ChantierBtn_Click);
+            // 
+            // MMBtn
+            // 
+            this.MMBtn.Location = new System.Drawing.Point(316, 25);
+            this.MMBtn.Name = "MMBtn";
+            this.MMBtn.Size = new System.Drawing.Size(172, 26);
+            this.MMBtn.TabIndex = 3;
+            this.MMBtn.Text = "Dossier MetalMind";
+            this.MMBtn.UseVisualStyleBackColor = true;
+            this.MMBtn.Click += new System.EventHandler(this.MMBtn_Click);
+            // 
+            // ChantierPathTxt
+            // 
+            this.ChantierPathTxt.Location = new System.Drawing.Point(19, 71);
+            this.ChantierPathTxt.Name = "ChantierPathTxt";
+            this.ChantierPathTxt.ReadOnly = true;
+            this.ChantierPathTxt.Size = new System.Drawing.Size(291, 26);
+            this.ChantierPathTxt.TabIndex = 1;
+            // 
+            // MMPathTxt
+            // 
+            this.MMPathTxt.Location = new System.Drawing.Point(19, 25);
+            this.MMPathTxt.Name = "MMPathTxt";
+            this.MMPathTxt.ReadOnly = true;
+            this.MMPathTxt.Size = new System.Drawing.Size(291, 26);
+            this.MMPathTxt.TabIndex = 0;
+            // 
+            // ConsoleListBox
+            // 
+            this.ConsoleListBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ConsoleListBox.FormattingEnabled = true;
+            this.ConsoleListBox.ItemHeight = 18;
+            this.ConsoleListBox.Location = new System.Drawing.Point(12, 209);
+            this.ConsoleListBox.Name = "ConsoleListBox";
+            this.ConsoleListBox.Size = new System.Drawing.Size(495, 130);
+            this.ConsoleListBox.TabIndex = 9;
+            // 
+            // BGWorker
+            // 
+            this.BGWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.resetBtn);
+            this.groupBox2.Controls.Add(this.MonthsListView);
+            this.groupBox2.Location = new System.Drawing.Point(513, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(110, 327);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mois finis";
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(6, 284);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(95, 37);
+            this.resetBtn.TabIndex = 12;
+            this.resetBtn.Text = "Reinitialiser";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // MonthsListView
+            // 
+            this.MonthsListView.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.MonthsListView.Location = new System.Drawing.Point(6, 25);
+            this.MonthsListView.MultiSelect = false;
+            this.MonthsListView.Name = "MonthsListView";
+            this.MonthsListView.Size = new System.Drawing.Size(95, 226);
+            this.MonthsListView.TabIndex = 11;
+            this.MonthsListView.UseCompatibleStateImageBehavior = false;
+            this.MonthsListView.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 348);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.ConsoleListBox);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transfer gestion de stock Metal Mind";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox MonthsComboBoxStart;
+        private System.Windows.Forms.Button TransferBtn;
+        private System.Windows.Forms.Button ChantierBtn;
+        private System.Windows.Forms.Button MMBtn;
+        private System.Windows.Forms.TextBox ChantierPathTxt;
+        private System.Windows.Forms.TextBox MMPathTxt;
+        private System.Windows.Forms.ListBox ConsoleListBox;
+        private System.ComponentModel.BackgroundWorker BGWorker;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView MonthsListView;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.ComboBox MonthsComboBoxEnd;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ transferGestionStockMetalMind/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace transferGestionStockMetalMind
+{
+    public partial class Form1 : Form
+    {
+        public String[] months ={"Janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre" };
+        String codeChantier;
+        FolderBrowserDialog FileBrowser = new FolderBrowserDialog();
+        String WriteCommand, DeleateCommand;
+        int InsertCount;
+        OleDbConnection SourceConnectionHandler,TargetConnectionHandler,DeleteConnectionHandler;
+        OleDbCommand GetDataQuery,insertCommand,ClearDataQuery;
+        DataTable ResultSet;
+
+        private String[] codes = new String[50];
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialiseData();
+        }
+        
+
+
+
+        // Main Code ----------------------------------------------------------------
+
+        private void initialiseData()
+        {
+            for (int j = 0; j < months.Length; j++)
+            {
+                // populate Ui
+                MonthsComboBoxStart.Items.Add(months[j]);
+                MonthsComboBoxEnd.Items.Add(months[j]);
+                Label MonthLabel = new Label();
+                MonthLabel.Text = "* " + months[j];
+                MonthsListView.Items.Add(months[j]);
+
+                // properties check
+                if (Properties.Settings.Default.MonthsDone == null)
+                    Properties.Settings.Default.MonthsDone = new bool[12];
+                // color Ui depending on months done
+                if (Properties.Settings.Default.MonthsDone[j])
+                    MonthsListView.Items[j].ForeColor = Color.Green;
+                else
+                    MonthsListView.Items[j].ForeColor = Color.Red;
+            }
+        }
+
+        private void TransferData(String MMFilePath, String codeChantier, String Month)
+        {
+
+            //Console.WriteLine(Month);
+            SourceConnectionHandler = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+ MMFilePath + ";Extended Properties=dBASE IV;User ID=Admin;Password=;");
+            ResultSet = new DataTable();
+
+            // Open the connection, and if open successfully, you can try to query it
+            SourceConnectionHandler.Open();
+            // collect TargetFolder relevant DATA from Source Folder
+            if (SourceConnectionHandler.State == ConnectionState.Open)
+            {
+                GetDataQuery = new OleDbCommand("select * from FMVT.DBF WHERE CTC='93" + codeChantier + "00' AND COP='S' AND DMV LIKE '__" + Month + "%' ; ", SourceConnectionHandler);
+                //GetDataQuery = new OleDbCommand("select * from FMVT.DBF WHERE SUBSTR (CTC, 0, 2) = '93' AND SUBSTRING (CTC, 2, 5) LIKE '" + codeChantier + "' AND COP='S' AND DMV LIKE '__" + Month + "%' ; ", SourceConnectionHandler);
+                new OleDbDataAdapter(GetDataQuery).Fill(ResultSet);
+                SourceConnectionHandler.Close();
+            }
+
+            InsertCount = 0;
+            if (ResultSet.Rows.Count > 0) // if found any relevant data to transfer
+            {
+
+                // deleate related data from current folder (to avoid duplication)
+                DeleteConnectionHandler = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ChantierPathTxt.Text + "\\" + codeChantier + ";Extended Properties=dBASE IV;User ID=Admin;Password=;");
+                ClearDataQuery = new OleDbCommand("DELETE from FMVT.DBF WHERE CTC='93" + codeChantier + "00' AND COP='E' AND DMV LIKE '__" + Month + "%' ; ", DeleteConnectionHandler);
+                DeleteConnectionHandler.Open();
+                if (DeleteConnectionHandler.State == ConnectionState.Open)
+                {
+                    try {ClearDataQuery.ExecuteNonQuery();}
+                    catch (Exception e){
+                        ShowFatalErrorMessage("Veuillez quitter tout les fichier .dbf ouvert : "+e.Message);
+                    }
+                    finally { DeleteConnectionHandler.Close();}
+                }
+
+
+                // get data from MetalMind file related to current folder
+                TargetConnectionHandler = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + ChantierPathTxt.Text + "\\" + codeChantier + ";Extended Properties=dBASE IV;User ID=Admin;Password=;");
+                foreach (DataRow row in ResultSet.Rows) 
+                {
+                    WriteCommand = "INSERT INTO FMVT.DBF ( COP, NBON , CART, DMV, QMV, PMV, CTC, CCF, IMP ) VALUES ( 'R', ? , ?, ?, ?, ?, ?, ?, ? ); ";
+
+                    insertCommand = new OleDbCommand(WriteCommand, TargetConnectionHandler);
+                    insertCommand.Parameters.Add("NBON", OleDbType.Char).Value = row["NBON"];
+                    insertCommand.Parameters.Add("CART", OleDbType.Char).Value = row["CART"];
+                    insertCommand.Parameters.Add("DMV", OleDbType.Char).Value = row["DMV"];
+                    insertCommand.Parameters.Add("QMV", OleDbType.Double).Value = row["QMV"];
+                    insertCommand.Parameters.Add("PMV", OleDbType.Double).Value = row["PMV"];
+                    insertCommand.Parameters.Add("CTC", OleDbType.Char).Value = row["CTC"];
+                    insertCommand.Parameters.Add("CCF", OleDbType.Char).Value = row["CCF"];
+                    insertCommand.Parameters.Add("IMP", OleDbType.Char).Value = row["IMP"];
+
+                    TargetConnectionHandler.Open();
+                    if (TargetConnectionHandler.State == ConnectionState.Open)
+                    {
+                        try{
+                            insertCommand.ExecuteNonQuery();
+                            InsertCount++;
+                        }
+                        catch (Exception e) {
+                            ShowMessage("Erreur d'ecriture : "+ insertCommand.CommandText + "            "+ codeChantier+
+                     "  NBON "+ row["NBON"]+
+                     "  CART "+ row["CART"]+
+                     "  DMV "+ row["DMV"]+
+                     "  QMV "+ row["QMV"]+
+                     "  PMV "+ row["PMV"]+
+                     "  CTC "+ row["CTC"]+
+                     "  CCF "+ row["CCF"]+
+                     "  IMP "+ row["IMP"]
+                            );
+                        }
+                        finally{ TargetConnectionHandler.Close(); }
+                    }
+                }
+                
+            }
+            PushToConsole("Chantier '" + codeChantier + "' fini avec " + InsertCount + " Insertion au mois de " + Int32.Parse(Month) );
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            
+
+        }
+
+
+
+
+        // User Events --------------------------------------------------------------
+
+        private void MMBtn_Click(object sender, EventArgs e)
+        {
+            MMPathTxt.Text = OpenFileBrowser("Choisissez le chemin du dossier MetalMind");
+        }
+
+        private void ChantierBtn_Click(object sender, EventArgs e)
+        {
+            ChantierPathTxt.Text = OpenFileBrowser("Choisissez le chemin des dossiers chantier");
+        }
+
+        private void TransferBtn_Click(object sender, EventArgs e)
+        {
+            // form validation
+            if (MMPathTxt.Text == "" || ChantierPathTxt.Text == "")
+            {
+                ShowMessage("veillez selectionner les repertoires");
+            }
+            else if (MonthsComboBoxStart.SelectedIndex == -1 || MonthsComboBoxEnd.SelectedIndex == -1)
+            {
+                ShowMessage("veillez selectionner les dates");
+            }
+            else if (MonthsComboBoxStart.SelectedIndex > MonthsComboBoxEnd.SelectedIndex)
+            {
+                ShowMessage("veillez rectifier la selection des dates");
+            }
+            else
+            {
+                if (File.Exists(@MMPathTxt.Text + "\\FMVT.DBF"))// if MetalMind file exist
+                {
+                    setInputEnabled(false);//disable input
+
+                    // foreach folder Name == nom chantier
+                    foreach (string folderPath in Directory.GetDirectories(@ChantierPathTxt.Text))
+                    {
+                        codeChantier = folderPath.Remove(0, folderPath.LastIndexOf("\\") + 1);// get folder name
+                        //Console.WriteLine();
+                        if (File.Exists(folderPath + "\\FMVT.DBF")) // if ChantierX file exist
+                        {
+                            //Console.WriteLine(MonthsComboBoxEnd.SelectedIndex - MonthsComboBoxStart.SelectedIndex + 1);
+                            if (MonthsComboBoxStart.SelectedIndex != MonthsComboBoxEnd.SelectedIndex)
+                            for (int m = MonthsComboBoxStart.SelectedIndex+1; m < MonthsComboBoxEnd.SelectedIndex+2; m++)
+                            {
+                                //Console.WriteLine(m);
+                                
+                                if(m < 10)
+                                    TransferData(@MMPathTxt.Text, codeChantier, '0' + m.ToString()); // all this to add to numbers less than 10 a '0'
+                                else
+                                    TransferData(@MMPathTxt.Text, codeChantier, m.ToString()); // all this to add to numbers less than 10 a '0'
+                                
+
+                                //TransferData(@MMPathTxt.Text, codeChantier, m < 10 ? ('0' + m.ToString()) : (m.ToString())); // all this to add to numbers less than 10 a '0'
+                                Properties.Settings.Default.MonthsDone[m-1] = true;
+                                MonthsListView.Items[m-1].ForeColor = Color.Green;
+                            }
+                            else
+                            {
+                                //Console.WriteLine(MonthsComboBoxStart.SelectedIndex+1);
+                                
+                                if(MonthsComboBoxStart.SelectedIndex + 1 < 10)
+                                    TransferData(@MMPathTxt.Text, codeChantier, '0' + (MonthsComboBoxStart.SelectedIndex + 1).ToString()); // all this to add to numbers less than 10 a '0'
+                                else
+                                    TransferData(@MMPathTxt.Text, codeChantier, (MonthsComboBoxStart.SelectedIndex + 1).ToString()); // all this to add to numbers less than 10 a '0'
+                                
+
+                                //TransferData(@MMPathTxt.Text, codeChantier, m < 10 ? ('0' + m.ToString()) : (m.ToString())); // all this to add to numbers less than 10 a '0'
+                                Properties.Settings.Default.MonthsDone[MonthsComboBoxStart.SelectedIndex ] = true;
+                                MonthsListView.Items[MonthsComboBoxStart.SelectedIndex].ForeColor = Color.Green;
+                            }
+                        }
+                        else
+                        {
+                            PushToConsole("Fichier FMVT du Chantier/dossier \"" + codeChantier + "\" inexistant");
+                        }
+                    }
+                    Properties.Settings.Default.Save();
+                    setInputEnabled(true);//enable input
+                }
+                else
+                {
+                    ShowMessage("Fichier MetalMind inexistant");
+                }
+            }
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            if (ShowConfirmatiomMessage("Êtes-vous sûr de vouloir reinitialiser ?",""))
+            {
+                for (int i = 0; i < Properties.Settings.Default.MonthsDone.Length; i++)
+                {
+                    Properties.Settings.Default.MonthsDone[i] = false;
+                    MonthsListView.Items[i].ForeColor = Color.Red;
+                }
+                Properties.Settings.Default.Save();
+            }
+        }
+
+
+
+
+        // UI -----------------------------------------------------------------------
+
+        // Show Msg in pop up
+        private void ShowMessage(String message, String title = "Erreur")
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void ShowFatalErrorMessage(String message, String title = "Erreur")
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(0);
+        }
+
+        // Show confirmation Msg in pop up
+        private bool ShowConfirmatiomMessage(String message, String title = "Erreur")
+        {
+            if (MessageBox.Show(message, title,
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                return true;
+            else
+                return false;
+        }
+
+        // Print int console
+        private void PushToConsole(String message)
+        {
+            ConsoleListBox.Items.Add(message);
+        }
+
+        // Show file browser
+        private String OpenFileBrowser(String description, String initialPath = "C:\\")
+        {
+            FileBrowser.SelectedPath = @initialPath;
+            FileBrowser.Description = description;
+            if (FileBrowser.ShowDialog() == DialogResult.OK)
+            {
+                return FileBrowser.SelectedPath;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        // Enable or disable input
+        private void setInputEnabled(bool isEnabled)
+        {
+            MonthsComboBoxStart.Enabled = isEnabled;
+            MonthsComboBoxEnd.Enabled = isEnabled;
+            MMBtn.Enabled = isEnabled;
+            ChantierBtn.Enabled = isEnabled;
+            TransferBtn.Enabled = isEnabled;
+            resetBtn.Enabled = isEnabled;
+        }
+    }
+}
